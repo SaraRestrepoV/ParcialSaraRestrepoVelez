d3db80605bcc4acadab23b9860b58598627434b6
Sara Restrepo Vélez <89092194+SaraRestrepoV@users.noreply.github.com>
2023-05-21 14:05:13 -0500

    Entities and databasecontext initial structure

---
+++ ParcialSaraRestrepoVelez/ParcialSaraRestrepoVelez/DAL/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ParcialSaraRestrepoVelez.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        { 
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ParcialSaraRestrepoVelez/ParcialSaraRestrepoVelez/DAL/Entities/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialSaraRestrepoVelez.DAL.Entities
+{
+    public class Ticket : Entity
+    {
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        [Display(Name = "Fue usada")]
+        public Boolean? IsUsed { get; set; }
+        [Display(Name = "Portería entrada")]
+        public string? EntranceGate { get; set; }
+    }
+}
